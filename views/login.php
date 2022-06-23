24635a104d55d7a2e883b419f33b845bd9e50bad
DSGabriels <gabrields_@outlook.com>
2022-06-22 22:19:56 -0300

    Update login.php

--- views/login.php
+++ views/login.php
@@ -40,7 +40,7 @@
                 <div class="col-md-6 col-lg-7 d-flex align-items-center">
                   <div class="card-body p-4 p-lg-5 text-black">
     
-                    <form>  
+                    <form method="post">  
                       <hr class="divider-horizontal-blurry" />
                       <div class="d-flex align-items-center mb-3 pb-1">
                         <i class="fas fa-cubes fa-4x me-3" style="color: #ff6219;"></i>
